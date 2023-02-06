77047adf499ef26b0a979495a0d90930eff978ac
princekumarpaswan <prasantandal123@gmail.com>
2023-02-06 22:35:33 +0530

    intial commit for internship Demo

---
+++ src/components/hero.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./hero.css"
+
+const Plan = () => {
+    return (
+        <>
+            <div>
+                <div className="d-flex flex-wrap mx-4  bg-primary textNav"  >
+
+                    <div className="d-flex align-items-center " >
+                        <span className="material-symbols-outlined fs-2 ">
+                            arrow_back_ios
+                        </span>
+                        <h4 className="mt-2 fs-5 mainNeg " >Restart</h4>
+                    </div>
+
+                    <div>
+                        <h1>Jetty</h1>
+                    </div>
+
+                    <div className="" >
+                        <span className="material-symbols-outlined">
+                            phone_enabled
+                        </span>
+                        <span className="material-symbols-outlined">
+                            sms
+                        </span>
+                    </div>
+
+
+
+
+
+
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Plan
